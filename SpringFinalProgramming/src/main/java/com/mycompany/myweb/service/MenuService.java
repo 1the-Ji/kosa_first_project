adf422cac4b1942b473b8e908d93c8b4bc540343
starrysara7 <starrysara@naver.com>
2016-11-11 11:31:27 +0900

    서비스 객체 생성(event, menu) & eventDao 오타수정 - 이보나

---
+++ SpringFinalProgramming/src/main/java/com/mycompany/myweb/service/MenuService.java
@@ -0,0 +1,53 @@
+package com.mycompany.myweb.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.mycompany.myweb.dao.MenuDao;
+import com.mycompany.myweb.dto.Menu;
+
+@Component
+public class MenuService {
+	public static final int WRITE_SUCCESS = 0;
+	public static final int WRITE_FAIL = 1;
+	
+	public static final int MODIFY_SUCCESS = 0;
+	public static final int MODIFY_FAIL = 1;
+	
+	public static final int REMOVE_SUCCESS = 0;
+	public static final int REMOVE_FAIL = 1;
+	
+	@Autowired
+	private MenuDao menuDao;
+	
+	public List<Menu> list(int pageNo, int rowsPerPage){
+		return menuDao.selectByPage(pageNo, rowsPerPage);
+	}
+	
+	public int write(Menu menu){
+		int row = menuDao.insert(menu);
+		return WRITE_SUCCESS;
+	}
+	
+	public int modify(Menu menu){
+		int row = menuDao.update(menu);
+		if(row == 0) { return MODIFY_FAIL; }
+		return MODIFY_SUCCESS;
+	}
+	
+	public int remove(int mid){
+		int row = menuDao.delete(mid);
+		if(row == 0) { return REMOVE_FAIL; }
+		return REMOVE_SUCCESS;
+	}
+	
+	public Menu info(int mid){
+		return menuDao.selectByMid(mid);
+	}
+	
+	public int getCount(){
+		return menuDao.count();
+	}
+}
