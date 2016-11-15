14bfd0b5efcc62599717cee13d79e2b7437f1f5f
seunghoom ji <seunghoom ji>
2016-11-15 21:02:21 +0900

    승훈

--- SpringFinalProgramming/src/main/java/com/mycompany/myweb/controller/HomeController.java
+++ SpringFinalProgramming/src/main/java/com/mycompany/myweb/controller/HomeController.java
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	//
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
 	@RequestMapping("/")
 	public String index(){
 		logger.info("index실행");
--- SpringFinalProgramming/src/main/java/com/mycompany/myweb/dao/EventDao.java
+++ SpringFinalProgramming/src/main/java/com/mycompany/myweb/dao/EventDao.java
@@ -2,13 +2,17 @@ package com.mycompany.myweb.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
+import com.mycompany.myweb.controller.HomeController;
 import com.mycompany.myweb.dto.Event;
 
 
@@ -16,6 +20,21 @@ import com.mycompany.myweb.dto.Event;
 public class EventDao {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	private static final Logger logger = LoggerFactory.getLogger(EventDao.class);
+	public List<Event> selectList(){
+		List<Event> list = new ArrayList<>();
+		logger.info("selectList() 2");
+		for(int i = 1;i<=10;i++){
+			Event event = new Event();
+		//	event.setBtitle("제목 :" + i);
+		//	event.setBcontent("내용 :" + i);
+		//	event.setBwriter("글쓴이 :" + i);
+			list.add(event);
+		}
+		logger.info("selectList() 3");
+		return list;
+		
+	}
 	
 	public int insert(Event event){
 		String sql = "insert into event(eid, estartperiod, elastperiod, econtents, esavedfile, emimetype, sid, mid) values(seq_event_eid.nextval, ?, ?, ?, ?, ?, ?, ?)";
@@ -55,7 +74,6 @@ public class EventDao {
 		return row;
 	}
 	
-	
 	public Event selectByEid(int eid){ 
 		String sql = "select eid, estartperiod, elastperiod, econtents, esavedfile, emimetype, sid, mid from event where eid=?";
 		List<Event> list = jdbcTemplate.query(sql, new Object[]{eid}, new RowMapper<Event>(){
@@ -76,6 +94,4 @@ public class EventDao {
 		});
 		return (list.size() != 0) ? list.get(0) : null;
 	}
-	
-	
 }
