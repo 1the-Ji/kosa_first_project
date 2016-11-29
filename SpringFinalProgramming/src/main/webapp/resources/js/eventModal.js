185d57bf464c9ae27741c92b5fb54e416693c987
blueskii <blueskii@naver.com>
2016-11-29 14:30:06 +0900

    .

--- SpringFinalProgramming/src/main/webapp/resources/js/eventModal.js
+++ SpringFinalProgramming/src/main/webapp/resources/js/eventModal.js
@@ -21,4 +21,27 @@ $(function() {
 	});
 });
 
+function eventList(){
+	$.ajax({
+		url:"event/eventList",
+		data:{},
+		success:function(data){
+			$("#eventTbody").empty();
+			for(var i=0; i<data.eventList.length; i++) {
+				var event = data.eventList[i];
+				$("#eventTbody").append(
+			         '<tr>'+
+		             '<td>'+ event.eid +'</td>' +
+		             '<td><a href="info?eid=${event.eid}">'+ event.etitle +'</td>'+
+		             '<td>'+ event.estartperiod + ' ~ ' + event.elastperiod + '</td>'+
+		             '<td>'+ event.econtents + '~' + '</td>'+
+		             '<td class="text-center"><a class="btn btn-info btn-xs" href="#"><span class="glyphicon glyphicon-edit"></span> Edit</a> <a href="#" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span> Del</a></td>'+
+			         '</tr>'
+            	);
+			}
+			$("#eventModal").modal("show");
+		}
+	});
+};
+
 
